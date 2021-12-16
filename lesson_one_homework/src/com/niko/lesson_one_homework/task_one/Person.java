9f6378fabed686eda366a044554a542c32a8eef1
siporqueno <strike1000001@gmail.com>
2021-12-16 18:41:00 +0300

    Task 1 of Homework of Lesson 1. Created class Person with builder in it. Removed class UserDto, which I created by mistake, without reading task carefully.

---
+++ lesson_one_homework/src/com/niko/lesson_one_homework/task_one/Person.java
@@ -0,0 +1,105 @@
+package com.niko.lesson_one_homework.task_one;
+
+public class Person {
+    private final String firstName, middleName, lastName, country, address, phone, gender;
+
+    private final int age;
+
+    public Person(Builder builder) {
+        this.firstName = builder.firstName;
+        this.middleName = builder.middleName;
+        this.lastName = builder.lastName;
+        this.country = builder.country;
+        this.address = builder.address;
+        this.phone = builder.phone;
+        this.age = builder.age;
+        this.gender = builder.gender;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+
+        private String firstName, middleName, lastName, country, address, phone, gender;
+
+        private int age;
+
+        public Builder withFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder withMiddleName(String middleName) {
+            this.middleName = middleName;
+            return this;
+        }
+
+        public Builder withLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder withCountry(String country) {
+            this.country = country;
+            return this;
+        }
+
+        public Builder withAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder withPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder withAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder withGender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
+        }
+    }
+}
